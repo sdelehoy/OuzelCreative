fe84275d13b587545bfaea84ab40712bff3e0908
sdelehoy <sdelehoy@icloud.com>
2023-02-10 13:19:27 -0700

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,31 @@
+// JavaScript Document
+
+// Update the copyright year
+document.getElementById("copyright-year").innerHTML = new Date().getFullYear();
+
+// Sideshows
+let slideIndex = [1, 1, 1, 1];
+let slideId = ["slides1", "slides2", "slides3", "slides4"];
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+showSlides(1, 3);
+
+function plusSlides(n, no) {
+	showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+	let i;
+	let x = document.getElementsByClassName(slideId[no]);
+	if (n > x.length) {
+		slideIndex[no] = 1;
+	}
+	if (n < 1) {
+		slideIndex[no] = x.length
+	}
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	x[slideIndex[no]-1].style.display = "block";
+}
