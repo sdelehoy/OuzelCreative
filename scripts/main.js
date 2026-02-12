aebf8f4a878ff5f551635f17d37851bd0293129f
sdelehoy <sdelehoy@icloud.com>
2026-02-12 16:42:45 -0700

    update

--- scripts/main.js
+++ scripts/main.js
@@ -150,7 +150,7 @@ class Footer extends HTMLElement {
               	class="social-link"
               	href="https://www.instagram.com/ouzelcreative/"
               	target="_blank"
-								rel="external nofollow"
+								rel="external"
             	>
             		<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               		<path fill="currentColor" d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4zm9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3"/>
@@ -161,7 +161,7 @@ class Footer extends HTMLElement {
               	class="social-link"
               	href="https://www.linkedin.com/company/ouzel-creative/"
               	target="_blank"
-								rel="external nofollow"
+								rel="external"
             	>
             		<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               		<path fill="currentColor" d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2zm-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93zM6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37z"/>
@@ -173,7 +173,7 @@ class Footer extends HTMLElement {
               	href="https://www.greengeeks.com"
               	target="_blank"
               	alt="GreenGeeks"
-              	rel="external nofollow"
+              	rel="external"
               >
 								<img
 									src="/images/Green_15.png"
@@ -191,7 +191,7 @@ class Footer extends HTMLElement {
 					<div class="flex space-between affiliates">
 						<div class="flex">
 							<div>
-								<a href="https://www.marketingmadesimple.com/Katy-Delehoy" target="_blank" rel="external nofollow">
+								<a href="https://www.marketingmadesimple.com/Katy-Delehoy" target="_blank" rel="external">
 									<img
 										width="104px"
 										src="/images/StoryBrand_Badge_2025_White.svg"
@@ -202,7 +202,7 @@ class Footer extends HTMLElement {
 								</a>
 							</div>
 							<div>
-								<a href="https://www.marketingmadesimple.com/Katy-Delehoy" target="_blank" rel="external nofollow">
+								<a href="https://www.marketingmadesimple.com/Katy-Delehoy" target="_blank" rel="external">
 									<img
 										width="104px"
 										src="/images/StoryBrand_Guide_Badge_White.png"
@@ -213,7 +213,7 @@ class Footer extends HTMLElement {
 								</a>
 							</div>
 							<div>
-								<a href="https://www.greenbiztracker.org/business/ouzel-creative-loveland" target="_blank" rel="external nofollow">
+								<a href="https://www.greenbiztracker.org/business/ouzel-creative-loveland" target="_blank" rel="external">
 									<img
 										width="104px"
 										src="/images/CGBN_Gold_Level_Stamp.png"
@@ -226,7 +226,7 @@ class Footer extends HTMLElement {
         		</div>
 						<div class="flex">
 							<div>
-            		<a href="https://www.bbb.org/us/co/loveland/profile/graphic-designer/ouzel-creative-llc-0805-46144719/#sealclick" target="_blank" rel="external nofollow">
+            		<a href="https://www.bbb.org/us/co/loveland/profile/graphic-designer/ouzel-creative-llc-0805-46144719/#sealclick" target="_blank" rel="external">
 									<img
 										src="https://seal-wynco.bbb.org/seals/gray-seal-96-50-bbb-46144719.png"
 										style="border: 0;"
@@ -237,7 +237,7 @@ class Footer extends HTMLElement {
 								</a>
           		</div>
           		<div>
-            		<a href="https://business.loveland.org/list/member/ouzel-creative-llc-14008" target="_blank" rel="external nofollow">
+            		<a href="https://business.loveland.org/list/member/ouzel-creative-llc-14008" target="_blank" rel="external">
               		<img
 										src="/images/Loveland-Chamber-Proud-member-Logo-Gray.png"
 										alt="Loveland Chamber of Commerce Member badge"
@@ -247,7 +247,7 @@ class Footer extends HTMLElement {
             		</a>
           		</div>
           		<div>
-            		<a href="https://www.lovelandcreates.org/listing/ouzel-creative/" target="_blank" rel="external nofollow">
+            		<a href="https://www.lovelandcreates.org/listing/ouzel-creative/" target="_blank" rel="external">
               		<img
 										src="/images/lcd-H-logo-tagline-gray.png"
 										alt="Loveland Creative District Member badge"
@@ -266,7 +266,7 @@ class Footer extends HTMLElement {
           	</div>
 						<div>&vert;</div>
 						<div>
-							<p>Images of Katy &amp; Shawn by <a target="_blank" href="https://www.danamartincreative.com" rel="external nofollow">Dana Martin Creative</a></p>
+							<p>Images of Katy &amp; Shawn by <a target="_blank" href="https://www.danamartincreative.com" rel="external">Dana Martin Creative</a></p>
 						</div>
           	<div>&vert;</div>
           	<div><a class="copyright-link" name="link" href="/privacy-policy" rel="privacy-policy">Privacy Policy</a></div>
